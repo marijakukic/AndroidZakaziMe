0e83408315d3b9c05b5f8773d99bbc449d3f9e5b
marijakukic <marijakukice2@gmail.com>
2019-04-12 19:27:31 +0200

    Tabs review

---
+++ app/src/main/java/com/example/marija/DetailViewActivity.java
@@ -0,0 +1,62 @@
+package com.example.marija;
+
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+import android.widget.TableLayout;
+import android.widget.TextView;
+
+public class DetailViewActivity extends AppCompatActivity {
+
+    private SectionPageAdapter sectionsPagerAdapter;
+    private ViewPager viewPager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_view);
+
+      //  Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+      //  setSupportActionBar(toolbar);
+
+        sectionsPagerAdapter= new SectionPageAdapter(getSupportFragmentManager());
+        viewPager=(ViewPager)findViewById(R.id.container);
+        setViewPager(viewPager);
+
+        TabLayout tabLayout = (TabLayout)findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+
+
+
+    }
+
+    private void setViewPager(ViewPager viewPager){
+        SectionPageAdapter adapter = new SectionPageAdapter(getSupportFragmentManager());
+        adapter.addFragment(new OpisTab(),"Opis");
+        adapter.addFragment(new RadnoVreme(),"Radno vreme");
+        adapter.addFragment(new Termini(),"Termini");
+        adapter.addFragment(new Recenzije(),"Recenzije");
+        viewPager.setAdapter(adapter);
+    }
+
+
+
+
+
+
+}
---
+++ app/src/main/java/com/example/marija/SectionPageAdapter.java
@@ -0,0 +1,41 @@
+package com.example.marija;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SectionPageAdapter extends FragmentPagerAdapter {
+
+    private final List<Fragment> mFragmentList = new ArrayList<>();
+    private final List<String> mFragmentTitle = new ArrayList<>();
+
+
+    public SectionPageAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    public void addFragment(Fragment fragment,String title){
+        mFragmentList.add(fragment);
+        mFragmentTitle.add(title);
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitle.get(position);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return mFragmentList.get(i);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+}
