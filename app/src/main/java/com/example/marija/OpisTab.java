5984c1b9817964e9906d6fa43937eb6a917788d3
Tamara Vrzic <vrzictamara@gmail.com>
2019-04-15 00:16:49 +0200

    tabovi opis i radno_vreme, pocetak implementacije mapa

--- app/src/main/java/com/example/marija/OpisTab.java
+++ app/src/main/java/com/example/marija/OpisTab.java
@@ -7,6 +7,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 public class OpisTab extends Fragment {
 
@@ -14,6 +15,10 @@ public class OpisTab extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.opis,container,false);
+
+
         return view;
     }
+
+
 }
--- app/src/main/java/com/example/marija/Termini.java
+++ app/src/main/java/com/example/marija/Termini.java
@@ -3,17 +3,74 @@ package com.example.marija;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
-public class Termini extends Fragment {
+import java.util.ArrayList;
 
-    @Nullable
+public class Termini extends AppCompatActivity  {
+
+    ArrayList<DataModel> dataModels;
+    ListView listView;
+    private static CustomAdapter adapter;
+
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.termini,container,false);
-        return view;
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.termini);
+
+        listView=(ListView)findViewById(R.id.list22);
+
+        dataModels= new ArrayList<>();
+
+        dataModels.add(new DataModel("Apple Pie", "Android 1.0", "1","September 23, 2008"));
+        dataModels.add(new DataModel("Banana Bread", "Android 1.1", "2","February 9, 2009"));
+        dataModels.add(new DataModel("Cupcake", "Android 1.5", "3","April 27, 2009"));
+        dataModels.add(new DataModel("Donut","Android 1.6","4","September 15, 2009"));
+        dataModels.add(new DataModel("Eclair", "Android 2.0", "5","October 26, 2009"));
+
+        adapter= new CustomAdapter(dataModels,getApplicationContext());
+
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                DataModel dataModel= dataModels.get(position);
+
+                Snackbar.make(view, dataModel.getName()+"\n"+dataModel.getType()+" API: "+dataModel.getVersion_number(), Snackbar.LENGTH_LONG)
+                        .setAction("No action", null).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
