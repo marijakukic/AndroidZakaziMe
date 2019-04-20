5a4fdb1ee18d20b97503bff7d3f8db35a7dd674a
Tamara Vrzic <vrzictamara@gmail.com>
2019-04-20 13:26:15 +0200

    mape VOL 2

---
+++ app/src/main/java/com/example/marija/DialogCenovnik.java
@@ -0,0 +1,31 @@
+package com.example.marija;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+
+public class DialogCenovnik extends AppCompatDialogFragment {
+    Button btn;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder =new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View view = inflater.inflate(R.layout.dialog_cenovnik,null);
+        btn = (Button)view.findViewById(R.id.cenovnik);
+        builder.setView(view).setTitle("Cenovnik")
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                // nista
+            }
+        });
+
+        return builder.create();
+
+    }
+}
